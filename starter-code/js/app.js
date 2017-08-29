06ab4f386ea56bdc6a4944f8a90ec177df2c6c60
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-08-29 12:53:10 +0200

    table

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -3,21 +3,3 @@ var player2 = new Player();
 var board = new Board();
 var question = new QuestionAndAnswers();
 var myDice =new Dice();
-
-
-var div = $("<div>");
-
- for(var i=0; i<40; i++){
- $("#main").append(div);
- }
-
-
-$( document ).ready(function() {
-
-   for(var i=0; i<42; i++){
-
-   $("#main").append("<div></div>");
-
-   }
-
- });
